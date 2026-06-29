410ef3d676e55facba67070fb1cd5b12e87b338f
jatin55-alt <thakurjatin2357@gmail.com>
2026-06-29 10:30:39 +0530

    Fix lint errors and format project

--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,5 @@ const router = getRouter();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
